e69adcfae154badafeda282b5984d5b06e95f0d0
wwalid18 <9833@holbertonstudents.com>
2024-12-16 10:49:41 +0100

    adjusts

--- sort.h
+++ sort.h
@@ -20,8 +20,13 @@ typedef struct listint_s
     const int n;
     struct listint_s *prev;
     struct listint_s *next;
-} listint_t; 
+} listint_t;
 
 void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
